1fd3c92400d3e2d7fdd1b65e82dcf005af9692e9
milac0 <milac0@hotmail.com>
2019-11-20 15:52:15 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuthRoute from "./components/AuthRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import AuthRoute from "./components/AuthRoute";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Fragment>
+      <Navbar />
+      <Router>
       <Switch>
         <AuthRoute path="/" component={Home} exact />
         <Route path="/login" component={Login} />
       </Switch>
     </Router>
+    </Fragment>
+
   );
 }
 
