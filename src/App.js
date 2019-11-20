b065ed9cadbbe5dafd05be2e0dd4c7a54e0cf7cc
milac0 <milac0@hotmail.com>
2019-11-20 15:42:43 +0100

    AuthRoute

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import AuthRoute from "./components/AuthRoute";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} exact />
+        <AuthRoute path="/" component={Home} exact />
         <Route path="/login" component={Login} />
       </Switch>
     </Router>
